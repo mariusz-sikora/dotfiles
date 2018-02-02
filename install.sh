aba6fa1283cbd982266f9890ede39f479ad43a8d
Mariusz Sikora <msikora87@gmail.com>
2018-02-02 21:29:01 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-#pass
+
+# Symlink .vimrc with dotfiles/vim/vimrc if not exists
+
+_current_path=$PWD
+
+if [ ! -f ~/.vimrc ]; then
+	ln -s $_current_path/vim/vimrc ~/.vimrc
+fi
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
